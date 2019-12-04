275b5f5fde0ea3c28f4245ce9559490ef621f885
Alfonso Tienda <atienda@iprocuratio.com>
2019-12-04 10:01:43 +0100

    assign and seek

--- src/main/java/ConsumerAS.java
+++ src/main/java/ConsumerAS.java
@@ -5,14 +5,15 @@ import java.util.concurrent.CountDownLatch;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Consumer {
+public class ConsumerAS {
 
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(Consumer.class);
+        Logger logger = LoggerFactory.getLogger(ConsumerAS.class);
 
         // Definimos las properties
 
@@ -22,17 +23,26 @@ public class Consumer {
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "ooo");
+      //  properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "ooo");
 
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+      //  properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         // Creamos el consumidor
 
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(properties);
 
-        kafkaConsumer.subscribe(Arrays.asList("primer-topic"));
+      //  kafkaConsumer.subscribe(Arrays.asList("primer-topic"));
 
-        // Leemos
+      TopicPartition topicPartition = new TopicPartition("primer-topic", 0);
+      kafkaConsumer.assign(
+          Arrays.asList(topicPartition)
+      );
+
+      long offset = 22L;
+
+      kafkaConsumer.seek(topicPartition, offset);
+
+   //     Leemos
         CountDownLatch latch = new CountDownLatch(1);
         final Runnable hilo = new ConsumerThread(latch, kafkaConsumer);
 
